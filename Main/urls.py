308e0a678c6bc866fc03eb36e4ba5487920c5e9d
Amal App <amalapp8@gmail.com>
2021-01-15 18:54:40 +0530

    Modified images

--- Main/urls.py
+++ Main/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views #from all import views	
 urlpatterns = [
-   path('', views, name='index'),
+   path('', views.index, name='index'),
+   path('about', views.about, name='about'),
+   path('contact', views.contact, name='contact'),
+   path('faq', views.faq, name='faq'),
+   path('shop', views.shop, name='shop'),
   ]
